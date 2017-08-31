71ff3f504b480d1d6e22c00cae7166868e3b33d8
fderue <francois.xavier.derue@gmail.com>
2017-08-31 18:05:41 -0400

    add env variable for amqp

--- pywps/processing/celery_joblauncher.py
+++ pywps/processing/celery_joblauncher.py
@@ -1,6 +1,12 @@
 from celery import Celery
 import os
-app = Celery('joblauncher', broker='pyamqp://guest@localhost//')
+
+amqp_user = os.getenv('AMQP_USER')
+amqp_password = os.getenv('AMQP_PASSWORD')
+amqp_hostname = os.getenv('AMQP_HOST')
+amqp_port = os.getenv('AMQP_PORT')
+
+app = Celery('joblauncher', broker='pyamqp://'+amqp_user+':'+amqp_password+'@'+amqp_hostname+':'+amqp_port+'//')
 
 
 @app.task
